d36aadcd5920d1867412e6e18671aed951bc9580
Waheed Aishat A <waheedaishat@gmail.com>
2023-05-19 23:29:54 +0100

    fix api

--- api/routers/user.js
+++ api/routers/user.js
@@ -6,53 +6,53 @@ const router = new express.Router()
 
  
 
-router.post('/register', async (req, res) => {
-    const { name, email, phoneNo } = req.body
-    const user = new User({
-        name,
-        email,
-        phoneNo
-      })   
-      try{
-        console.log(name, email, phoneNo)
-        const savedUser = await user.save();
-        console.log('User saved to database:', savedUser);
-        res.status(201).send(savedUser);
-      }catch(error) {
-        console.error('Error saving user to database:', error);
-        res.status(500).send('Internal Server Error');
-      }
-})
+// router.post('/register', async (req, res) => {
+//     const { name, email, phoneNo } = req.body
+//     const user = new User({
+//         name,
+//         email,
+//         phoneNo
+//       })   
+//       try{
+//         console.log(name, email, phoneNo)
+//         const savedUser = await user.save();
+//         console.log('User saved to database:', savedUser);
+//         res.status(201).send(savedUser);
+//       }catch(error) {
+//         console.error('Error saving user to database:', error);
+//         res.status(500).send('Internal Server Error');
+//       }
+// })
 
-router.post('/login', async (req, res) => {
-    try {
-        console.log(req.body.email);
-      const user = await User.findOne({email : req.body.email})
-      console.log(user);
-      const token = await user.generateAuthToken()
-      console.log(token);
-      res.send({ user, token})
-    } catch (error) {
-      res.status(400).send({error})
-     }
-    })
+// router.post('/login', async (req, res) => {
+//     try {
+//         console.log(req.body.email);
+//       const user = await User.findOne({email : req.body.email})
+//       console.log(user);
+//       const token = await user.generateAuthToken()
+//       console.log(token);
+//       res.send({ user, token})
+//     } catch (error) {
+//       res.status(400).send({error})
+//      }
+//     })
 
 
 
-    //logout
-router.post('/logout', auth, async (req, res) => {
+//     //logout
+// router.post('/logout', auth, async (req, res) => {
    
-    try {
-       req.user.tokens =  req.user.tokens.filter((token) => {
-            return token.token !== req.token 
-        })
+//     try {
+//        req.user.tokens =  req.user.tokens.filter((token) => {
+//             return token.token !== req.token 
+//         })
 
-        await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send()
-    }
-})
+//         await req.user.save()
+//         res.send()
+//     } catch (error) {
+//         res.status(500).send()
+//     }
+// })
 
 //Logout All 
 router.post('/logoutAll', auth, async(req, res) => {
